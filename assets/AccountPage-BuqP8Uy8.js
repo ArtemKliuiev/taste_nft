511ce88547e7879aea1f025c2640e732bccd592d
artem <mr_artem>
2024-04-10 16:05:47 +0300

    Updates

---
+++ assets/AccountPage-BuqP8Uy8.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o,a as t}from"./index-FhLAtx8k.js";const n={},s=t("h1",null,"Акаунт",-1),a=[s];function _(r,l){return o(),c("div",null,a)}const f=e(n,[["render",_]]);export{f as default};
