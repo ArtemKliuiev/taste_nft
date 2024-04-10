5946d22125f99dcf42350989757d5bb5d50bad92
artem <mr_artem>
2024-04-10 16:49:28 +0300

    Updates

--- assets/AccountPage-Bl4_O6oY.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o,a as t}from"./index-Bg3wdbA8.js";const n={},s=t("h1",null,"Акаунт",-1),a=[s];function _(r,l){return o(),c("div",null,a)}const f=e(n,[["render",_]]);export{f as default};
