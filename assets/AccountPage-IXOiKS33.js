fc54cb16c7ae630c340866cd84c8fd6da1fc2f06
artem <mr_artem>
2024-04-19 20:40:18 +0300

    Updates

--- assets/AccountPage-IXOiKS33.js
+++
@@ -1,1 +0,0 @@
-import{f as e,c,o,h as t}from"./index-CDChAm7t.js";const n={},s=t("h1",null,"Акаунт",-1),a=[s];function r(_,l){return o(),c("div",null,a)}const f=e(n,[["render",r]]);export{f as default};
