77f7e0298eefec7065f0f7ee212850b24863b272
artem <mr_artem>
2024-04-19 20:57:34 +0300

    Updates

---
+++ assets/AccountPage-aops44AB.js
@@ -0,0 +1,1 @@
+import{f as e,c,o,h as t}from"./index-Tez5tBvx.js";const n={},s=t("h1",null,"Акаунт",-1),a=[s];function r(_,l){return o(),c("div",null,a)}const f=e(n,[["render",r]]);export{f as default};
